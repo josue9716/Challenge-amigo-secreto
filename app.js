73705a265ccc268455b35435441642ef2c82aea1
Josue De la Cruz Mercedes <josuedelacruzmercedes16@outlook.com>
2025-03-09 11:04:28 -0400

    Crea funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let nombreImput = document.getElementById('amigo').value.trim();
+
+    if(nombreImput === ""){
+        alert('Por favor, inserte un nombre.');
+        return;
+    } else{
+
+        amigos.push(nombreImput);
+        document.getElementById('amigo').value = "";
+    }
+
+}
+
+function actualizarLista(){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML ="";
+
+    for ( let i = 0; i < amigos.length; i++){
+        lista.innerHTML += `<li>${amigos[i]}</li>`;
+    }
+}
+
+function sortearAmigo(){
+    if (amigos.lenght === 0){
+        document.getElementById('resultado').innerHTML = "No hay amigos en la lista para sortear.";
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() *amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    document.getElementById('resultado').innerHTML = `Amigo seleccionado: <strong>${amigoSorteado}</strong>`;
+
+}
